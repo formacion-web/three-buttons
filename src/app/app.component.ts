eb8c87a56f2b92b5512642eaecab4e6ca43abe45
prueba <you@mail.com>
2021-06-15 14:03:57 +0200

    solucion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'three-buttons';
+  textoAzul!:string;
+  textoRojo!:string;
+  textoVerde!:string;
+  textoDisable = 'este botón no funciona ahora';
+  textoEnable = ['Saludar','Desactivar','Clica aquí para Activar'];
+  disable = true;
+
+  // textoBotones = {
+  //   botonVerde: {enable:'Clica aquí para Activar',disable:''},
+  //   botonRojo:  {enable:'Desactivar',disable:''},
+  //   botonAzul:  {enable:'Saludar',disable:''}
+  // }
+  constructor(){
+    this.textoRojo = this.textoAzul = this.textoDisable;
+    this.textoVerde = this.textoEnable[2];
+
+  }
+saludar(){
+  alert('HOLA CRACK');
+}
+buttonDisable(){
+  this.disable = !this.disable;
+
+}
+accionBotonVerde(){
+  this.buttonDisable();
+  this.textoVerde = this.textoDisable;
+  this.textoAzul = this.textoEnable[0];
+  this.textoRojo = this.textoEnable[1];
+}
+
+accionBotonRojo(){
+  this.buttonDisable();
+  this.textoRojo = this.textoDisable;
+  this.textoVerde = this.textoEnable[2];
+  this.textoAzul = this.textoDisable;
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule } from "@angular/material/button";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
